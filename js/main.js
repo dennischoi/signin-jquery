c7a5297694ed37b3c731e536283b6531457c91ff
Dennis Choi <dennis.choi7@gmail.com>
2016-04-25 23:21:53 -0400

    Bonus modal fadeout... event.stopPropagation()

--- js/main.js
+++ js/main.js
@@ -4,22 +4,36 @@ $(document).on('ready', function() {
     $('.modal').fadeIn();
   });
 
-  $('.close').on('click', function() {
+  $('.close').on('click', function(event) {
     $('.modal').fadeOut();
+    event.stopPropagation();
+  });
+// BONUS
+
+  $('.modal').on('click', function() {
+    $('.modal').fadeOut();
+    // event.stopPropagation();
   });
 
-  $('.submit').on('click', function() {
+
+  $('.submit').on('click', function(event) {
     $("input").addClass("error");
+    // .stopPropagation has to be after the event since it reads top down
+    event.stopPropagation();
     // $('.error').error(function() {
       alert("ERROR... Calling the popo...")
     // });
   });
 
-  $('input').on('mouseenter', function() {
-    $("input").removeClass("error");
+  // $('input').on('mouseenter', function() {
+  //   $("input").removeClass("error");
+  // });
+
+  $('input').on('click', function(event) {
+    $('input').removeClass("error");
+    event.stopPropagation();
   });
 
-  $()
 
 
 
